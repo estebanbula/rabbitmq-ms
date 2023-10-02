c8f60bd8a6822bff12fe77eab7c6ed42a45b5ef4
Esteban Bula <carlosbula01@gmail.com>
2023-10-01 22:19:49 -0500

    rabbit integration microservices

---
+++ notifications-ms/src/test/java/com/estebanbula/notifications/NotificationsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estebanbula.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ orders-ms/src/test/java/com/estebanbula/orders/OrdersMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estebanbula.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
